938b19741736e25fb9ad38a766a9824e66f6ddf5
Mevludin Causevic <mevludincausevic@gmail.com>
2023-02-23 11:08:51 -0700

    added functionality for lvl 0`

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
 
-const App = (props) => (
-  <div>Hello World!</div>
-);
+var movies = [
+  {title: 'Mean Girls'},
+  {title: 'Hackers'},
+  {title: 'The Grey'},
+  {title: 'Sunshine'},
+  {title: 'Ex Machina'},
+];
 
+const App = () => {
+  const [query, setQuery] = React.useState('');
+  const [queryResults, setQueryResults] = React.useState([]);
+
+  // handle search event
+
+  return (
+    <ul>
+      {movies.map((movie, index) => {
+        return <MovieItem key={index} movieName={movie.title}/>
+      })}
+    </ul>
+  )
+};
+
+function MovieItem({ movieName }) {
+  return <li>{movieName}</li>
+}
+
+function search() {
+
+}
 export default App;
+
